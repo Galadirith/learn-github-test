6532fa36e38de9f8fb278a5b807c215b64b20d87
Edward Fauchon-Jones <edward.fauchon.jones@gmail.com>
2018-07-24 03:06:06 +0100

    Update nb 1-3

--- learn-github-setup/2-manage-projects-with-git.sh
+++ learn-github-setup/2-manage-projects-with-git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo 'Removing /learn-github/2-manage-projects-with-git'
-rm -rf /learn-github/2-manage-projects-with-git
-echo 'Removing /learn-github/.2-manage-projects-with-git.git'
-rm -rf /learn-github/.2-manage-projects-with-git.git
-echo 'Creating /learn-github/.2-manage-projects-with-git.git'
-git init --bare /learn-github/.2-manage-projects-with-git.git
+echo 'Removing 2-manage-projects-with-git'
+rm -rf 2-manage-projects-with-git
+echo 'Removing .2-manage-projects-with-git.git'
+rm -rf .2-manage-projects-with-git.git
+echo 'Creating .2-manage-projects-with-git.git'
+git init --bare .2-manage-projects-with-git.git
